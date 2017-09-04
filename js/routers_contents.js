d9348186ff0defffec49dfcfda0508e48a4e4716
Leonel Hernandez <leonelphm@gmail.com>
2017-09-04 06:16:42 -0400

    Agregado planteo general

--- js/routers_contents.js
+++ js/routers_contents.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
                 $("#introduccion").load("contenido/introduccion/index.html"); 
                 $("#Cap1").load("contenido/cap1/index.html"); 
+                $("#Cap1-continuacion").load("contenido/cap1/planteo_general_continuacion.html"); 
 });
