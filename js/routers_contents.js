1627a1cd5c5e6c6f047c5c2f6113fd182ce0c134
rudmanmrrod <rudman22@hotmail.com>
2017-09-04 08:35:27 -0400

    Nueva estructura de directorios, agregada falacias económicas y 80% de la tabla de proyectos pueblocentricos y empresocentricos

--- js/routers_contents.js
+++ js/routers_contents.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-                $("#introduccion").load("contenido/introduccion/index.html"); 
-                $("#Cap1").load("contenido/cap1/index.html"); 
-                $("#Cap1-continuacion").load("contenido/cap1/planteo_general_continuacion.html"); 
+$(document).ready(function() {   
+    $("#portada").load("contenido/0_portada/index.html");
+    $("#introduccion").load("contenido/1_introduccion/index.html"); 
+    $("#Cap1").load("contenido/2_cap1/index.html");
+    $("#Cap1-continuacion").load("contenido/2_cap1/planteo_general_continuacion.html");
+	$("#falacias_1").load("contenido/3_falacias/parte1.html"); 
+	$("#falacias_2").load("contenido/3_falacias/parte2.html"); 
+	$("#pn_1").load("contenido/6_proyectos_nacionales/tabla_1.html");
+	$("#pn_2").load("contenido/6_proyectos_nacionales/tabla_2.html");
+	$("#pn_3").load("contenido/6_proyectos_nacionales/tabla_3.html"); 
+	$("#pn_4").load("contenido/6_proyectos_nacionales/tabla_4.html");
 });
