a257cb3501efaf15cd1c1e6f70e6f7143f0e9951
lhernandez <lhernandez@cenditel.gob.ve>
2017-09-07 10:32:44 -0400

    Agregado esquema en el planteo general el cual describe las 7 tesis planteada por Oscar Varsavky en las cuales define como debe plantearse el Proyecto Nacional y la viabilidad del mismo

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,6 @@
 function loaddata_recursos(){
 	var datos = {
       'name': 'Recursos Disponibles',
-      'title': 'general manager',
       'children': [
         { 'name': 'Significado del cálculo de viabilidad',
 		'children':[
@@ -25,4 +24,51 @@ function loaddata_recursos(){
     $('#recursos').orgchart({
 		'data' : datos,
 	});
+};
+
+function loaddata_tesis(){
+    var datos = {
+      'name': 'Planteamiento de Objetivos y Viabilidad',
+      'children': [
+        { 'name': 'Descripcion del Poryecto Nacional',
+          'children':[
+            { 'name': 'Tesis 1',
+              'title': 'Los objetivos nacionales deben plantearse en términos \
+              de necesidades humanas, individuales y colectivas, materiales o \
+              “espirituales”; de todo tipo.'},
+            { 'name': 'Tesis 2',
+              'title': 'La lista de las necesidades que la sociedad debe \
+               considerar para satisfacerlas en algún grado o ninguno.'},
+            { 'name': 'Tesis 3',
+              'title': 'Cada posible propuesta, opción, meta o alternativa \
+               para satisfacer en algún grado una necesidad, debe ser tan \
+               explicita y concreta.'},
+        ]},     
+        { 'name': 'Viabilidad',
+          'children':[
+            { 'name': 'Tesis 4',
+              'title': 'Se estudia si hay alguna estrategia, o manera de \
+                        satisfacer todas las metas, utilizando los recursos \
+                        disponibles al comienzo y los que se vayan creando.',
+            'children':[{ 
+              'name': 'Tesis 5',
+              'title': 'Si tiene viabilidad física, \
+                       se ensaya su viabilidad social.'},
+             {'name': 'Tesis 6',
+              'title': 'Con una estrategia tecnológica y distributiva, se \
+              calculan los indicadores de viabilidad política.'},
+            ]},
+            
+           { 'name': 'Tesis 7',
+              'title': 'Este método sirve para evaluar Proyectos Nacionales y \
+               hallar estrategias tecnológicas y distributivas que los hagan \
+               viables.'},
+        ]},
+      ]
+    };
+    $('#tesis').orgchart({
+        'data' : datos,
+        'nodeContent': 'title',
+        //'direction': 'l2r'
+    });   
 }
--- js/routers_contents.js
+++ js/routers_contents.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#portada").load("contenido/0_portada/index.html");
     $("#introduccion").load("contenido/1_introduccion/index.html"); 
     $("#Cap1").load("contenido/2_cap1/index.html");
-    $("#Cap1-continuacion").load("contenido/2_cap1/planteo_general_continuacion.html");
+    //$("#Cap1-continuacion").load("contenido/2_cap1/planteo_general_continuacion.html");
 	$("#falacias_1").load("contenido/3_falacias/parte1.html"); 
 	$("#falacias_2").load("contenido/3_falacias/parte2.html"); 
 	$("#falacias_3").load("contenido/3_falacias/parte3.html");
@@ -13,4 +13,5 @@ $(document).ready(function() {
 	$("#pn_5").load("contenido/6_proyectos_nacionales/tabla_5.html");
 	$("#pn_6").load("contenido/6_proyectos_nacionales/tabla_6.html");
 	loaddata_recursos();
+    loaddata_tesis();
 });
